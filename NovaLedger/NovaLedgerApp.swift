2062c8c41fb2ac6fe0b353b24465148e7b89485b
Emmanuel Ahishakiye <eahishak@u.rochester.edu>
2025-11-29 20:44:45 -0500

    Initial Commit

---
+++ NovaLedger/NovaLedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  NovaLedgerApp.swift
+//  NovaLedger
+//
+//  Created by Ahishakiye, Emmanuel on 11/29/25.
+//
+
+import SwiftUI
+
+@main
+struct NovaLedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
