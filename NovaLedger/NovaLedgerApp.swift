788c0b24168b3b9c04826e2bb439a9d77e37f05e
Emmanuel Ahishakiye <eahishak@u.rochester.edu>
2025-11-29 23:45:32 -0500

    Polished API handling, improved readability, added documentation comments

--- NovaLedger/NovaLedgerApp.swift
+++ NovaLedger/NovaLedgerApp.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 @main
 struct NovaLedgerApp: App {
+    
+    init() {
+        // Request notification permissions on app launch
+        NotificationManager.shared.requestPermission()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .preferredColorScheme(getUserColorScheme())
         }
+    }
+    
+    // Get user's preferred color scheme from settings
+    private func getUserColorScheme() -> ColorScheme? {
+        let darkMode = UserDefaults.standard.bool(forKey: "darkMode")
+        return darkMode ? .dark : .light
     }
 }
